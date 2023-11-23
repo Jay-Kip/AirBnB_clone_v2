17e6112df38220cc006e4d40aa673ebad6d26e64
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-23 06:15:28 +0300

    Modified test case test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -97,3 +97,7 @@ class test_basemodel(unittest.TestCase):
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
+
+        import time
+        time.sleep(1)
+        new.save()
