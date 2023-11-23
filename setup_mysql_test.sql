06651b8802f653d7f70d27d13c9e333efd810e9a
HamO001 <ndarugastiv9@gmail.com>
2023-11-23 12:51:16 -0800

    overdue

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,32 +1,8 @@
-'''-- Create database named 'hbnb_test_db'
--- Create new user called 'hbnb_test' in localhost
--- Set password of new user to 'hbnb_test_pwd'
--- Grant all privileges to new user on this database
--- Grant SELECT privileges to new user to 'perfomance_schema'
--- Flush privileges
-
-
-
--- Create database
+-- create database hbnb_test_db if not exists
+-- create user hbnb_test with pwd hbnb_test_pwd
+-- grant hbnb_test select on performance_schema
+-- grant hbnb_test all privileges on hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
---Add new user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant all privileges to new user on this database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- Grant SELECT privileges
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';;
-
--- Flush privileges
-FLUSH PRIVILEGES;
-'''
-
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
