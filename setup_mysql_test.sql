a24a390d1d3c07be401963ffc652e448bac5d8a4
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-23 06:14:35 +0300

    a script that prepares a MySQL server for the project by creating database, adding user and giving privileges to thr user

---
+++ setup_mysql_test.sql
@@ -0,0 +1,23 @@
+-- Create database named 'hbnb_test_db'
+-- Create new user called 'hbnb_test' in localhost
+-- Set password of new user to 'hbnb_test_pwd'
+-- Grant all privileges to new user on this database
+-- Grant SELECT privileges to new user to 'perfomance_schema'
+-- Flush privileges
+
+
+
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnh_test_db;
+
+--Add new user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges to new user on this database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privileges
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;
