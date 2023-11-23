187c74f7bb2178412d2e9c51d6d10fbcfc07d461
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-23 19:23:40 +0300

    Script that creates a database and new user andgrants privileges to the user

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -16,7 +16,7 @@ CREATE USER  IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privileges
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to apply changes immediately
 FLUSH PRIVILEGES;
